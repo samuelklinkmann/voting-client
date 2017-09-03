2ed9e8efd801f9022898bc07768f94fcf48cd9ea
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2017-09-03 12:53:35 +0300

    Initial commit

---
+++ src/components/Voting.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default React.createClass({
+    getPair: function() {
+        return this.props.pair || [];
+    },
+
+    isDisabled: function() {
+        return !!this.props.hasVoted;
+    },
+    hasVotedFor: function(entry) {
+        return this.props.hasVoted === entry;
+    },
+    render: function() {
+        return <div className="voting">
+            {this.props.winner ?
+            <div ref="winner">Winner is {this.props.winner}!</div> :
+            this.getPair().map(entry =>
+                <button key={entry} 
+                        onClick={() => this.props.vote(entry)}
+                        disabled={this.isDisabled()}>
+                    <h1>{entry}</h1>
+
+                    {this.hasVotedFor(entry) ?
+                    <div className="label">Voted</div> : null}
+                </button>
+            )}
+            </div>;
+    }
+})
